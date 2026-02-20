1b2ea54a8e32e11fb94930115cb2808e8c104df7
hamza-codefied <hamza.codefied123@gmail.com>
2026-02-20 06:37:33 -0600

    room edit fix

--- hooks/useUpdateRoom.tsx
+++ hooks/useUpdateRoom.tsx
@@ -12,8 +12,8 @@ export const useUpdateRoom = () => {
       roomId: string;
       roomData: any;
     }) => {
-      // Remove _id before sending
-      const { _id, ...payload } = roomData;
+      // Remove _id, currentOccupancy, currentKids, and status before sending
+      const { _id, currentOccupancy, currentKids, status, ...payload } = roomData;
 
       const res = await api.patch(`/room/${roomId}`, payload);
       return res.data.room;
